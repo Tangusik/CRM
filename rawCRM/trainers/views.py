ae84ae0edd2ce06e135367c9f8dc8f30f6a03a32
kaver <kaverin14456576ivan@gmail.com>
2023-02-04 00:20:39 +0300

    Добавлена система пользователей

--- rawCRM/trainers/views.py
+++ rawCRM/trainers/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from .models import Client
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
     client_list = Client.objects.order_by()
-    context = {'client_list': client_list}
+
+    if request.user.is_authenticated:
+        context = {'client_list': client_list, 'userinfo': request.user}
+    else:
+        context = {'client_list': client_list}
+
     return render(request, 'trainers/base.html', context)
+
+
+def log_in(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def log_out(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
