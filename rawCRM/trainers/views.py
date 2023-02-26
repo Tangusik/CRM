bf09efcc0d985380952c5d0608b91bf3ff732756
kaver <kaverin14456576ivan@gmail.com>
2023-02-26 13:53:38 +0300

    начало маршрутизации

--- rawCRM/trainers/views.py
+++ rawCRM/trainers/views.py
@@ -5,15 +5,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 
-def index(request):
-    client_list = Client.objects.order_by()
-
-    if request.user.is_authenticated:
-        context = {'client_list': client_list, 'userinfo': request.user}
+def login_page(request):
+    if  request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('main'))
     else:
-        context = {'client_list': client_list}
+        return render(request, 'trainers/login.html')
 
-    return render(request, 'trainers/base.html', context)
 
 
 def log_in(request):
@@ -22,9 +19,23 @@ def log_in(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-    return HttpResponseRedirect(reverse('index'))
+        return HttpResponseRedirect(reverse('login_page'))
+    else:
+        return HttpResponseRedirect(reverse('main'))
 
 
 def log_out(request):
     logout(request)
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('login_page'))
+
+
+def main(request):
+    if request.user.is_authenticated:
+        userinfo = request.user
+        context = {'userinfo': userinfo}
+        return render(request, 'trainers/main.html', context)
+    else:
+        return HttpResponseRedirect(reverse('login_page'))
+
+def clients(request):
+    pass
